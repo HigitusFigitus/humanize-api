e6cf463c69cfd4cb18fa67c72269cffe6113ac95
Ilaria Varriale <ilariasvarriale@gmail.com>
2016-11-06 18:16:01 -0800

    Adds root to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # constraints subdomain: 'api' do
   namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
+      root "responses#index"
       resources :responses,  only: [:index, :create, :show]
       resources :companies,  only: [:index, :create, :show]
       resources :responders, only: [:index, :create, :show]
