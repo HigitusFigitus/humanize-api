b82e3cbaa37edec4ce281c545344d89c3c63222c
Ilaria Varriale <ilariasvarriale@gmail.com>
2016-11-07 22:37:56 -0800

    Adds scope to model

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,6 +1,13 @@
 class Response < ApplicationRecord
-  scope :by_value, -> (value) {where("value = ?", value)}
-
   belongs_to :responder
   belongs_to :question
+
+  scope :by_value, -> (value) {where("value = ?", value)}
+  scope :before, -> (before) {where("before = ?", before)}
+
+
+  def by_question(question_body)
+    question = Question.find_by(body: question_body)
+    responses = Response.where(question_id: question.id)
+  end
 end
