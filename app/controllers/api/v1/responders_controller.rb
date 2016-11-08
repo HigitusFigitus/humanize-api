96122ff380af63d3ca90e5306a636021c9c65240
Ilaria Varriale <ilariasvarriale@gmail.com>
2016-11-08 11:26:46 -0800

    Builds JSON of one Responder model with corresponding Responses

--- app/controllers/api/v1/responders_controller.rb
+++ app/controllers/api/v1/responders_controller.rb
@@ -3,56 +3,19 @@ class Api::V1::RespondersController < ApplicationController
   def index
     if params[:before]
       responders = Responder.before(params[:before])
-
-      # Convert array of responder objects to array of hashes
-      responders_hashes = responders.as_json
-
-      # Iterate through each Responder object in responder variable defined above
-        # For each Responder, query the Response model for responses where the responder is the iteratee responder
-        # Collect all responses inside of an array
-      responses = responders.map {|responder| Response.where(responder: responder)}
-
-      # For each responder hash, insert new key/value pair where key is 'responses' and value is corresponding response from array defined above
-      responders_hashes_with_responses = responders_hashes.each_with_index.map do |responder, index|
-        responder["responses"] = responses[index]
-        responder
-      end
-
-      render json: ApiResponse.createResponse(request, responders_hash_with_responses)
-
-      # render json: { status: "SUCCESS", message: "Loaded all responders", data: responders }, status: :ok
+      data = ApiResponse.buildRespondersJson(responders)
+      render json: ApiResponse.createResponse(request, data)
     else
       responders = Responder.all
-      responders_hashes = responders.as_json
-      responses = responders.map {|responder| Response.where(responder: responder)}
-      responders_hashes_with_responses = responders_hashes.each_with_index.map do |responder, index|
-        responder["responses"] = responses[index]
-        responder
-      end
-
-      render json: ApiResponse.createResponse(request, responders_hashes_with_responses)
+      data = ApiResponse.buildRespondersJson(responders)
+      render json: ApiResponse.createResponse(request, data)
     end
   end
 
   def show
-
-    # p request.methods
-    # p "*****************"
-    # p request.fullpath
-
-    p responder = Responder.find(params[:id])
-    p questions = Response.where(:responder => responder)
-
-
-
-    render json: ApiResponse.createResponse(
-      request,
-        {
-          responder: responder,
-          questions: questions
-        }
-      )
-    # render json: @responders, status: :ok
+    responder = Responder.find(params[:id])
+    data = ApiResponse.buildResponderJson(responder)
+    render json: ApiResponse.createResponse(request, data)
   end
 
   def create
