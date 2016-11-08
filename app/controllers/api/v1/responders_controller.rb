3c7c89420c6dd5eda50001062c4768fab977adb1
Ilaria Varriale <ilariasvarriale@gmail.com>
2016-11-08 11:14:56 -0800

    Builds JSON of Responder models with corresponding Responses

--- app/controllers/api/v1/responders_controller.rb
+++ app/controllers/api/v1/responders_controller.rb
@@ -3,10 +3,34 @@ class Api::V1::RespondersController < ApplicationController
   def index
     if params[:before]
       responders = Responder.before(params[:before])
-      render json: { status: "SUCCESS", message: "Loaded all responders", data: responders }, status: :ok
+
+      # Convert array of responder objects to array of hashes
+      responders_hashes = responders.as_json
+
+      # Iterate through each Responder object in responder variable defined above
+        # For each Responder, query the Response model for responses where the responder is the iteratee responder
+        # Collect all responses inside of an array
+      responses = responders.map {|responder| Response.where(responder: responder)}
+
+      # For each responder hash, insert new key/value pair where key is 'responses' and value is corresponding response from array defined above
+      responders_hashes_with_responses = responders_hashes.each_with_index.map do |responder, index|
+        responder["responses"] = responses[index]
+        responder
+      end
+
+      render json: ApiResponse.createResponse(request, responders_hash_with_responses)
+
+      # render json: { status: "SUCCESS", message: "Loaded all responders", data: responders }, status: :ok
     else
       responders = Responder.all
-      render json: { status: "SUCCESS", message: "Loaded all responders", data: responders }, status: :ok
+      responders_hashes = responders.as_json
+      responses = responders.map {|responder| Response.where(responder: responder)}
+      responders_hashes_with_responses = responders_hashes.each_with_index.map do |responder, index|
+        responder["responses"] = responses[index]
+        responder
+      end
+
+      render json: ApiResponse.createResponse(request, responders_hashes_with_responses)
     end
   end
 
