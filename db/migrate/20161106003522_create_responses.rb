ef660944239fb18230e9f581a7c4aaaa8ac4d78e
spark <spark@MacBook-Air-2.local>
2016-11-08 16:42:16 -0800

    Format spacing correctly

--- db/migrate/20161106003522_create_responses.rb
+++ db/migrate/20161106003522_create_responses.rb
@@ -1,7 +1,7 @@
 class CreateResponses < ActiveRecord::Migration[5.0]
   def change
     create_table   :responses do |t|
-      t.integer     :value
+      t.integer    :value
       t.references :responder, foreign_key: true
       t.references :question, foreign_key: true
 
