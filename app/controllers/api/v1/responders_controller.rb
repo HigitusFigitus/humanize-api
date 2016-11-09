361d7d9dba5321589e295d60737c4e1579538f82
spark <spark@MacBook-Air-2.local>
2016-11-08 17:26:29 -0800

    Add query string parsing logic to responders#index

--- app/controllers/api/v1/responders_controller.rb
+++ app/controllers/api/v1/responders_controller.rb
@@ -1,16 +1,30 @@
 class Api::V1::RespondersController < ApplicationController
 
   def index
-    if params[:before]
-      responders = Responder.where(session_id: params[:session_id]).before(params[:before])
+    if params[:before] || params[:gender] || params[:age_group] || params[:position]
+      responders = params[:before] ? Responder.where(session_id: params[:session_id]).before(params[:before]) : Responder.where(session_id: params[:session_id])
+      responders = params[:gender] ? responders.where(gender: params[:gender]) : responders
+      if params[:age_group]
+        case params[:age_group]
+        when 1
+          responders = responders.where("age > ?", 17).where("age < ?", 26)
+        when 2
+          responders = responders.where("age > ?", 25).where("age < ?", 41)
+        when 3
+          responders = responders.where("age > ?", 40).where("age < ?", 66)
+        when 4
+          responders = responders.where("age > ?", 64)
+        end # ends case
+      end # ends if age_group
+      responders = params[:position] ? responders.where(position: params[:position]) : responders
+
       data = ApiResponse.buildRespondersJson(responders)
       render json: ApiResponse.createResponse(request, data)
     else
-      p params[:session_id]
       responders = Responder.where(session_id: params[:session_id])
       data = ApiResponse.buildRespondersJson(responders)
       render json: ApiResponse.createResponse(request, data)
-    end
+    end # ends if checking for params
   end
 
   def show
