53d7e6b2fe5f9d8f2e39b1491d522063ef2d10b8
Young Ju Park <parkyngj@gmail.com>
2016-11-10 12:29:46 -0800

    Changes seed company name from Dropbox to Bloopr

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,12 +27,12 @@ def get_id(objects_with_ids)
   objects_with_ids.map { | object_with_id | object_with_id.id }
 end
 
-dropbox = Company.create(name: "Dropbox")
+bloopr = Company.create(name: "Bloopr")
 
-teamwork_session = dropbox.sessions.create(date: Date.new(2016, 10, 21), topic: "Teamwork and 360° Feedback", content: "Bloop")
-inner_critic_session = dropbox.sessions.create(date: Date.new(2016, 10, 28), topic: "Inner Critic", content: "Bloop")
-difficult_conversations_session = dropbox.sessions.create(date: Date.new(2016, 11, 04), topic: "Difficult Conversations", content: "Bloop")
-allyship_session = dropbox.sessions.create(date: Date.new(2016, 11, 11), topic: "Allyship", content: "Bloop")
+teamwork_session = bloopr.sessions.create(date: Date.new(2016, 10, 21), topic: "Teamwork and 360° Feedback", content: "Bloop")
+inner_critic_session = bloopr.sessions.create(date: Date.new(2016, 10, 28), topic: "Inner Critic", content: "Bloop")
+difficult_conversations_session = bloopr.sessions.create(date: Date.new(2016, 11, 04), topic: "Difficult Conversations", content: "Bloop")
+allyship_session = bloopr.sessions.create(date: Date.new(2016, 11, 11), topic: "Allyship", content: "Bloop")
 
 # allyship responders before
 
